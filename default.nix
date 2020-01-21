9c113c950ecfbbcc4942d5816def3ba0b5df66f3
Eric Rasmussen <erasmussen@cars.com>
2020-01-21 13:20:50 -0800

    clarify newScope intent

--- default.nix
+++ default.nix
@@ -1,6 +1,12 @@
 { pkgs ? import <nixpkgs> {} }:
 let
-   #callPackage = pkgs.lib.callPackageWith (pkgs // self);
+
+   # nix voyager is primarily used as a library to build docker-based
+   # derivations. in practice this means there will be some other package set
+   # with its own `pkgs` reference and `callPackage` function that loads this
+   # library. `newScope` is used here to make sure `pkgs` is kept separate,
+   # following examples in nixpkgs. if it ends up being unnecessary we can
+   # replace it with a more standard callPackage definition
    callPackage = pkgs.newScope ( pkgs // self);
 
    version = "0.1";
